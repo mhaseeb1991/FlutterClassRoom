1909aca0ad8918a8b0d8579df23cdea0d9db94a5
Mhaseeb1991 <mohammadhaseeb1991@gmail.com>
2022-03-15 19:59:29 +0500

    Assignment 4 Completed - Comments Added

--- Assignment4/Assignment4.dart
+++ Assignment4/Assignment4.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 void main() {
-  // question1();
+  //Uncomment the specific questionNo method and run the file. Output will be printed in the console.
+  question1();
   // question2();
   // question3();
   // question4();
@@ -12,7 +13,7 @@ void main() {
   // question9();
   // question10();
   // question11();
-  question12();
+  // question12();
   // question13();
   // question14();
 }
